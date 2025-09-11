7843cbbef7d4d3f2e25cde6ff8fcc680c4672913
Mikael Åberg <mikael.aberg@me.com>
2025-09-11 12:32:06 +0200

    fix: update import statement for TimeRangeManager in index.tsx and add README.md

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,5 @@
 import ScheduledView from './scheduled-view';
-import TimeRange from './span-time-range';
+import TimeRangeManager from './time-range-manager';
 
 const FULL_WEEK = [0, 1, 2, 3, 4, 5, 6];
 const WEEK_DAYS = [1, 2, 3, 4, 5];
@@ -7,5 +7,5 @@ const WEEKENDS = [0, 6];
 
 export * from './types';
 export { FULL_WEEK, WEEK_DAYS, WEEKENDS };
-export { TimeRange };
+export { TimeRangeManager };
 export default ScheduledView;
